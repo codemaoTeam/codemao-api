a56b7992a4d69287b6ff4d63276f0fc02ae98bd5
dagoyounger <dagoyounger@gmail.com>
2022-07-29 13:36:08 +0800

    fix: 修复评论区无法加载和编辑文档链接错误的问题

--- static/js/plugin.js
+++ static/js/plugin.js
@@ -1,6 +1,6 @@
 function plugin(hook, vm) {
   hook.beforeEach(function (content) {
-    const editUrl = `${vm.config.repo}/edit/master/${vm.route.file}`;
+    const editUrl = `${vm.config.repo}/edit/master${vm.route.file}`;
     const newContent = `!> 免责声明：请合理使用编程猫社区API，如果因为使用不当对您和编程猫社区造成损失，本文档开发团队不承担任何法律责任
 
 [:memo:编辑文档](${editUrl})
